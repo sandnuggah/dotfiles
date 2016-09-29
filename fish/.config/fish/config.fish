44bfa466e64e879a9646e8850c7c4e8b25cf1b04
sandnuggah <aaa@hueueueueu.eu>
2016-09-29 15:24:29 +0200

    Add files

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,15 @@
+set -x PATH /bin $PATH
+set -x PATH /sbin $PATH
+set -x PATH /usr/bin $PATH
+set -x PATH /usr/sbin $PATH
+set -x PATH /usr/local/bin $PATH
+set -x PATH /usr/local/sbin $PATH
+
+set -x EDITOR atom
+
+set -e fish_greeting
+
+alias gs='git status'
+alias gl='git log'
+alias gb='git branches'
+alias gd='git diff'
---
+++ readme.md
@@ -0,0 +1,10 @@
+# dotfiles
+
+## Installation
+
+```sh
+$ git clone https://github.com/sandnuggah/dotfiles.git ~/.dotfiles
+$ cd ~/.dotfiles && sh install
+```
+## Credits
++ Me
