9246372e6fe16e0e71d1dabd4d013049ca43b888
0x4d <0x4d@undefined>
2017-11-05 13:04:28 +0100

    Install direnv

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -8,6 +8,7 @@ set -x PATH ~/.local/bin $PATH
 set -x PATH /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/bin $PATH
 
 set -x EDITOR atom
+set -x HOMEBREW_GITHUB_API_TOKEN 1a5834b1a987e759d6980c0b518cf6a608207863
 
 set -e fish_greeting
 
@@ -15,3 +16,5 @@ alias gs='git status'
 alias gl='git log'
 alias gb='git branches'
 alias gd='git diff'
+
+eval (direnv hook fish)
