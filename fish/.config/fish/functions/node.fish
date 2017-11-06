bf2c73c215b78adb99cd9edcae07246fb42303c0
0x4d <0x4d@undefined>
2017-11-06 21:39:39 +0100

    Update description

--- fish/.config/fish/functions/node.fish
+++ fish/.config/fish/functions/node.fish
@@ -1,3 +1,3 @@
-function node --description 'alias docker node'
+function node --description 'run node inside of a docker container'
 	docker run -it --rm -v $PWD:/usr/src/app -w /usr/src/app node:8.8-slim node $argv;
 end
