81202507550d417e16859175d1eed2986fc175e1
0x4d <0x4d@undefined>
2017-11-08 11:39:38 +0100

    Use latest version of node

--- fish/.config/fish/functions/node.fish
+++ fish/.config/fish/functions/node.fish
@@ -1,3 +1,3 @@
 function node --description 'run node inside of a docker container'
-	docker run -it --rm -v $PWD:/usr/src/app -w /usr/src/app node:8.8-slim node $argv;
+	docker run -it --rm -v $PWD:/usr/src/app -w /usr/src/app node:slim node $argv;
 end
