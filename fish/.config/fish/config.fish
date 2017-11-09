be0dc46dc9c68f01f29bb0a65e07f2bc09a7fc26
0x4d <0x4d@undefined>
2017-11-09 13:58:49 +0100

    Add comments

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,6 +4,7 @@ if not functions -q fundle; eval (curl -sfL https://git.io/fundle-install); end
 fundle plugin 'tuvistavie/fish-kubectl'
 fundle plugin 'tuvistavie/fish-fastdir'
 
+# Setup paths
 set -x PATH /bin $PATH
 set -x PATH /sbin $PATH
 set -x PATH /usr/bin $PATH
@@ -11,6 +12,12 @@ set -x PATH /usr/sbin $PATH
 set -x PATH /usr/local/bin $PATH
 set -x PATH /usr/local/sbin $PATH
 set -x EDITOR atom
+
+# Load secrets (like GITHUB_API_TOKEN)
 source ~/.config/fish/secrets.fish
+
+# Load gcloud sdk paths
 source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc
+
+# Load direnv
 eval (direnv hook fish)
