65a8041f0fda796e009d65b87fdf8582876e57e7
0x4d <0x4d@undefined>
2017-11-13 15:33:56 +0100

    Initialize fundle

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,6 +3,7 @@ if not functions -q fundle; eval (curl -sfL https://git.io/fundle-install); end
 
 fundle plugin 'tuvistavie/fish-kubectl'
 fundle plugin 'tuvistavie/fish-fastdir'
+fundle init
 
 # Setup paths
 set -x PATH /bin $PATH
