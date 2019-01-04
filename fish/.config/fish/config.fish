581b9b090bdd5836887ebf81bf5622f5a72a86b2
0x4d <0x4d@undefined>
2019-01-04 15:06:54 +0100

    Remove sbin from PATH

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -12,7 +12,6 @@ set -x PATH /sbin $PATH
 set -x PATH /usr/bin $PATH
 set -x PATH /usr/sbin $PATH
 set -x PATH /usr/local/bin $PATH
-set -x PATH /usr/local/sbin $PATH
 
 # Abbreviations
 abbr -a dc 'docker-compose'
