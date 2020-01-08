3d45903f76af9767df0f9fe778c5c7c138d5428e
0x4d <0x4d@undefined>
2020-01-08 15:50:46 +0100

    Fix macOS 10.15: slow command autocompletion

---
+++ fish/.config/fish/functions/__fish_describe_command.fish
@@ -0,0 +1,9 @@
+# macOS 10.15 "Catalina" has *issues*
+if test (uname) = Darwin
+    set -l darwin_version (uname -r | string split .)
+    # macOS 15 is Darwin 19
+    if test "$darwin_version[1]" = 19 -a "$darwin_version[2]" -le 3
+        function __fish_describe_command; end
+        exit
+    end
+end
