02dbf574e2a6ef4da0995c5b3af4d88c836f489e
0x4d <0x4d@undefined>
2020-04-06 13:52:31 +0200

    Fix Darwin issue

--- fish/.config/fish/functions/__fish_describe_command.fish
+++ fish/.config/fish/functions/__fish_describe_command.fish
@@ -2,7 +2,7 @@
 if test (uname) = Darwin
     set -l darwin_version (uname -r | string split .)
     # macOS 15 is Darwin 19
-    if test "$darwin_version[1]" = 19 -a "$darwin_version[2]" -le 3
+    if test "$darwin_version[1]" = 19 -a "$darwin_version[2]" -le 4
         function __fish_describe_command; end
         exit
     end
