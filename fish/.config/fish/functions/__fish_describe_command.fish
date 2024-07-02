7e4829730538b724770415a61c2b30132b2532cd
0x4d <0x4d@undefined>
2024-07-02 11:22:09 +0200

    chore: Update macOS 10.15 "Catalina" compatibility code

--- fish/.config/fish/functions/__fish_describe_command.fish
+++ fish/.config/fish/functions/__fish_describe_command.fish
@@ -1,9 +1,3 @@
-# macOS 10.15 "Catalina" has *issues*
-if test (uname) = Darwin
-    set -l darwin_version (uname -r | string split .)
-    # macOS 15 is Darwin 19
-    if test "$darwin_version[1]" = 20 -a "$darwin_version[2]" -le 3
-        function __fish_describe_command; end
-        exit
-    end
-end
+# macOS 10.15 "Catalina" has *issues
+function __fish_describe_command; end
+exit
