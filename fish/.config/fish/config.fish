0b9c74af97064d04333af171d552f04977f368fa
0x4d <0x4d@undefined>
2024-09-24 15:47:55 +0200

    Remove conda initialize

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -55,3 +55,16 @@ eval (starship init fish)
 
 # asdf
 source /usr/local/opt/asdf/libexec/asdf.fish
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /opt/miniconda3/bin/conda
+    eval /opt/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+else
+    if test -f "/opt/miniconda3/etc/fish/conf.d/conda.fish"
+        . "/opt/miniconda3/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "/opt/miniconda3/bin" $PATH
+    end
+end
+# <<< conda initialize <<<
